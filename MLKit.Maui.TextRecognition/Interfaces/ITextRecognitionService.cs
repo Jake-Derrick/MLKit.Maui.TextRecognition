24a62265c820cca4c99327d5bc058c9b27f96737
Jake <jakee2000jake@gmail.com>
2024-06-26 13:21:21 -0500

    Create project and add text recognition service

---
+++ MLKit.Maui.TextRecognition/Interfaces/ITextRecognitionService.cs
@@ -0,0 +1,17 @@
+﻿namespace MLKit.Maui.TextRecognition;
+
+/// <summary>
+/// Interface for Google ML Kit Text Recognition Services
+/// </summary>
+public interface ITextRecognitionService
+{
+    /// <summary>
+    /// Extracts text from an image file using text recognition.
+    /// </summary>
+    Task<TextRecognitionResult> GetTextFromImage(FileResult imageFile);
+
+    /// <summary>
+    /// Extracts text from image bytes using text recognition.
+    /// </summary>
+    Task<TextRecognitionResult> GetTextFromImage(byte[] imageBytes);
+}
---
+++ MLKit.Maui.TextRecognition/Services/TextRecognitionService.cs
@@ -0,0 +1,14 @@
+﻿namespace MLKit.Maui.TextRecognition;
+
+#if !ANDROID && !IOS
+// See the Platforms folder for the device specific implementation of this service.
+/// <summary>
+/// The plain .Net implementation of <see cref="ITextRecognitionService"/>
+/// </summary>
+public class TextRecognitionService : ITextRecognitionService
+{
+    Task<TextRecognitionResult> ITextRecognitionService.GetTextFromImage(FileResult imageFile) => throw new NotImplementedException();
+
+    Task<TextRecognitionResult> ITextRecognitionService.GetTextFromImage(byte[] imageBytes) => throw new NotImplementedException();
+}
+#endif
