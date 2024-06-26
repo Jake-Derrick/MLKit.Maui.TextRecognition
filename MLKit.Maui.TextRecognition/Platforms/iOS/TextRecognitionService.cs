2212b3f6472609f012457b46fdc7266469c1203e
Jake Derrick <60721064+Jake-Derrick@users.noreply.github.com>
2024-06-26 13:22:13 -0500

    Create project and add text recognition service (#1)

---
+++ MLKit.Maui.TextRecognition/Platforms/iOS/TextRecognitionService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MLKit.Maui.TextRecognition;
+
+public class TextRecognitionService : ITextRecognitionService
+{
+    public Task<TextRecognitionResult> GetTextFromImage(FileResult imageFile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TextRecognitionResult> GetTextFromImage(byte[] imageBytes)
+    {
+        throw new NotImplementedException();
+    }
+}
